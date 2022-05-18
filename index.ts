c5b5765ac875bd6675db2857858a6001fa0baf6d
Bourek, Daniel <bouredan@fel.cvut.cz>
2022-05-18 23:54:34 +0200

    Revert "introduce rollup"
    This reverts commit 6745ad56db4c645b2ea4188bff6236ad20114fb6.

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from "./src/SfsApi";
+export * from "./src/Events";
+
+export * from "./src/facets/Facet"
+export * from "./src/facets/SelectFacet"
+export * from "./src/facets/CheckboxFacet"
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./SfsApi";
-export * from "./Events";
-
-export * from "./facets/Facet"
-export * from "./facets/SelectFacet"
-export * from "./facets/CheckboxFacet"
